db7dce3df6472c3bcee1fc8f5538d02c4cf535d3
stampsetthasit <setthasit.developer@gmail.com>
2022-12-08 21:59:58 +0700

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+const corsOptions = {
+    origin: '*',
+    exposedHeaders: 'Authorization'
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,7 +17,7 @@ app.use("/public/images", express.static(path.join(__dirname, '/public/images'))
 app.use('/api', require('./src/middlewares/api'));
 
 const host = 'localhost';
-const port = 3000;
+const port = 8080;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}. | http://${host}:${port}`);
 });
